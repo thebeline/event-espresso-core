88f4c245ddf91c615db2194974bd234e98eab05a
Darren Ethier <darren@roughsmootheng.in>
2016-12-09 12:13:51 -0500

    registering datetime template in our app js [touch: 10291]

--- admin_pages/experiments/assets/eventsexperiment.js
+++ admin_pages/experiments/assets/eventsexperiment.js
@@ -1,6 +1,7 @@
 (function(){
     'use strict';
     //modify the components we're using
+    eejs.api.components.Datetime.template = eejs.templates.datetime;
     eejs.api.components.Event.template = eejs.templates.event;
     eejs.api.EventCollection = Vue.extend(eejs.api.components.EventCollection);
     eejs.api.eeEvents = new eejs.api.EventCollection({
