5a338daca64e8a858f002ef1fc8c204b113eea8f
Darren Ethier <darren@roughsmootheng.in>
2016-12-09 17:03:45 -0500

    use the vue instance on eejs.api [touch: 10291]

--- admin_pages/experiments/assets/eventsexperiment.js
+++ admin_pages/experiments/assets/eventsexperiment.js
@@ -3,7 +3,7 @@
     //modify the components we're using
     eejs.api.components.Datetime.template = eejs.templates.datetime;
     eejs.api.components.Event.template = eejs.templates.event;
-    eejs.api.EventCollection = Vue.extend(eejs.api.components.EventCollection);
+    eejs.api.EventCollection = eejs.vue.extend(eejs.api.components.EventCollection);
     eejs.api.eeEvents = new eejs.api.EventCollection({
         el: '#app',
         components : {
