9aa97537a0a264a0cf2f5e058f945301ffa2ccbf
Darren Ethier <darren@roughsmootheng.in>
2016-12-09 21:23:23 -0500

    correct reference to data object [touch: 10291]

--- admin_pages/experiments/assets/eventsexperiment.js
+++ admin_pages/experiments/assets/eventsexperiment.js
@@ -1,8 +1,8 @@
 (function(){
     'use strict';
     //modify the components we're using
-    eejs.api.components.Datetime.template = eejs.templates.datetime;
-    eejs.api.components.Event.template = eejs.templates.event;
+    eejs.api.components.Datetime.template = eejs.data.templates.datetime;
+    eejs.api.components.Event.template = eejs.data.templates.event;
     eejs.api.EventCollection = eejs.vue.extend(eejs.api.components.EventCollection);
     eejs.api.eeEvents = new eejs.api.EventCollection({
         el: '#app',
