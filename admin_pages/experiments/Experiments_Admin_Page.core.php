fc06e4e33b20ac0919e2a2a7313df55f186d163d
Darren Ethier <darren@roughsmootheng.in>
2016-12-19 15:01:45 -0500

    fixes to registering templates [touch: 10291]

--- admin_pages/experiments/Experiments_Admin_Page.core.php
+++ admin_pages/experiments/Experiments_Admin_Page.core.php
@@ -97,14 +97,17 @@ class Experiments_Admin_Page extends EE_Admin_Page
             true
         );
         wp_enqueue_script('eventexperiment');
-
-        EE_Registry::$eejs['templates'] = array(
-            'event' => EEH_Template::display_template(
+        EE_Registry::instance()->AssetsRegistry->addTemplate(
+            'event',
+            EEH_Template::display_template(
                 EE_EXPERIMENTS_ADMIN_TEMPLATE_PATH . 'event_template.html',
                 '',
                 true
-            ),
-            'datetime' => EEH_Template::display_template(
+            )
+        );
+        EE_Registry::instance()->AssetsRegistry->addTemplate(
+            'datetime',
+            EEH_Template::display_template(
                 EE_EXPERIMENTS_ADMIN_TEMPLATE_PATH . 'datetime_template.html',
                 '',
                 true
